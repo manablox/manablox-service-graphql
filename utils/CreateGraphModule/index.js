9568e4e7ccb58d75a1a03c67f287901edeaaee16
daspete <daspetemail@gmail.com>
2020-01-28 20:07:08 +0100

    refactor: using destructor for creating graph modules

--- utils/CreateGraphModule/index.js
+++ utils/CreateGraphModule/index.js
@@ -4,10 +4,7 @@ export default (graph) => {
     let graphData = graph
     if(typeof graph.module !== 'undefined') graphData = graph.module
 
-    let moduleData = {}
-    if(graphData.typeDefs) moduleData.typeDefs = graphData.typeDefs
-    if(graphData.resolvers) moduleData.resolvers = graphData.resolvers
-    if(graphData.imports) moduleData.imports = graphData.imports
+    let moduleData = { ...graphData }
     
     let moduleContext = {}
     if(graphData.context) moduleContext = graphData.context 
